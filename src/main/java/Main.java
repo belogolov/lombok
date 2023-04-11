d57a2c3e546b99c4deae2f492ed76edca42e94f7
Timur Belogolov <belogolov.timur@yandex.ru>
2023-04-11 20:10:13 +0300

    add logback

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,45 +1,45 @@
 import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.core.ObjectCodec;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
 import lombok.SneakyThrows;
 import model.Berry;
 import model.Fruit;
 import model.Vegetable;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Main {
+    private static final Logger log = LoggerFactory.getLogger(Main.class);
     @SneakyThrows
     public static void main(String[] args) {
         Fruit apple = new Fruit();
         apple.setId(1);
         apple.setName("Apple");
-        System.out.println(apple);
+        log.info(apple.toString());
 
         Vegetable tomato = Vegetable.creator()
                 .withId(2)
                 .withName("Tomato")
                 .create();
-        System.out.println(tomato);
+        log.info(tomato.toString());
 
         String json = "{\"title\":\"Blueberry\",\"id\":3}";
 
         var objectMapper = newObjectMapper();
         Berry blueberry = objectMapper.readValue(json, Berry.class);
-        System.out.println(blueberry);
-        System.out.println(blueberry.equals(Berry.builder()
+        log.info(blueberry.toString());
+        log.info(String.format("Object equals - %s", blueberry.equals(Berry.builder()
                 .id(3)
                 .name("Blueberry")
-                .build()));
+                .build())));
 
         Berry wildberry = Berry.builder()
                 .id(5)
                 .build();
 
-        System.out.println(wildberry);
+        log.info(wildberry.toString());
         String json2 = objectMapper.writeValueAsString(wildberry);
-        System.out.println(json2);
+        log.info(json2);
 
     }
 
